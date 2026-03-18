260dd3cc0fc86937696ce6bdac6969baaaecfc46
kevin18ramos <kevin.18ramos04@gmail.com>
2026-03-18 13:00:41 -0500

    Adding base connection layout

---
+++ Postgres/connection.py
@@ -0,0 +1,35 @@
+import psycopg2
+from psycopg2 import OperationalError
+from psycopg2 import pool
+import argparse
+
+
+
+
+
+def connection(USER,PASSWORD,db_to_use):
+    try:
+
+        db_pool = pool.SimpleConnectionPool(
+            1, 4,
+            host="localhost",
+            database=db_to_use,
+            user=USER,
+            password=PASSWORD
+        )
+
+        # Get connection from pool
+        connection = db_pool.getconn()
+
+        # Create cursor
+        cursor = connection.cursor()
+
+        return cursor
+
+    except OperationalError as e:
+        print("Connection failed")
+        print(e)
+        exit(0)
+
+
+
---
+++ Postgres/task.py
@@ -0,0 +1,4 @@
+import connection as cn
+
+def connection_run(db,user,pwsd):
+    conn = cn.connection(db,user,pwsd)
