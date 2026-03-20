759d2cf879fae4fb5c56af49e511ce8c170925ff
kevin18ramos <kevin.18ramos04@gmail.com>
2026-03-20 11:29:29 -0500

    testing sql script

--- gcc.postgres/task.py
+++ gcc.postgres/task.py
@@ -1,13 +1,41 @@
 import cn as cn
 
-def insert_statement_s(schema_name,table_name,value_dict):
-    #for loop statement
-    ...
+def insert_statement_s(schema_name, table_name, dict_list):
+    insert_sql_list = []
 
-def di_table(caller_file,schema_name,table_name,value_dict,cn):
+    for value_dict in dict_list:
+        columns = ", ".join(value_dict.keys())
+
+        values = []
+        for value in value_dict.values():
+            if value is None:
+                values.append("NULL")
+            elif isinstance(value, str):
+                escaped = value.replace("'", "''")
+                values.append(f"'{escaped}'")
+            else:
+                values.append(str(value))
+
+        values_sql = ", ".join(values)
+
+        single_insert = f"""
+        INSERT INTO {schema_name}.{table_name} ({columns})
+        VALUES ({values_sql});
+        """
+        insert_sql_list.append(single_insert.strip())
+
+    return "\n".join(insert_sql_list)
+
+
+def di_table(caller_file, schema_name, table_name, dict_list):
     sql = f"""
     BEGIN;
-            {insert_statement_s(schema_name,table_name,value_dict)}
-            COMMIT;
-            """
-    cn.d_cn(caller_file)
+    {insert_statement_s(schema_name, table_name, dict_list)}
+    COMMIT;
+    """
+
+    print(sql)
+
+    #conn,cursor = cn.d_cn(caller_file)
+    #cursor.execute(sql)
+    #conn.commit()
